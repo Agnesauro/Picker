a608d5893633e55775b3834e4ccf06dc3ec787f9
Agnes Aurora Marinho da Silva Cavalcanti <vas0037579@aluno.firjansenaisesi.com.br>
2026-07-01 21:24:09 -0300

    almost there

--- src/Components/ComponentPicker.jsx
+++ src/Components/ComponentPicker.jsx
@@ -3,7 +3,16 @@ import { ScrollView, StyleSheet, Text, View, Button, Image, FlatList } from 'rea
 import { Picker } from 'react-native-web';
 
 export default function ComponentPicker(){
+    const imagensPizza = {
+        calabresa: 'https://www.receitasnestle.com.br/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/4ef71e576dd2c48bc6f075632240504b.webp?itok=GJauJyaW',
+        portuguesa: 'https://www.receitasnestle.com.br/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/2eb7ece4ae9a67b773aa138589e2031d.webp?itok=8rB5qKP-',
+        bacon: 'https://www.seara.com.br/wp-content/uploads/2025/09/R0325-SL-pizza-de-bacon-fatiado-cebola-caramelizada-1200x675-1.webp',
+        frango_catupiry: 'https://www.receitasnestle.com.br/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/85edcfdb1dae604c2627a88c06fa0c85.webp?itok=GL3lIKtV',
+        alho: ''
+    }
+
     const [sabor, setSabor] = useState('')
+
 
     return(
         <View style ={styles.container}>
